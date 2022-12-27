4b7010324dc909cd095a3a04bd9b68301a2d1690
reda awad <redaawad622@gmail.com>
2022-12-27 07:16:45 +0200

    locale-provider

--- pages/index.vue
+++ pages/index.vue
@@ -13,20 +13,22 @@ const submitForm = async () => {
 </script>
 
 <template>
-  <v-form lazy-validation ref="catForm" @submit.prevent="submitForm()">
-    <CategoryMain />
-    <v-btn
-      :loading="submitting.loading"
-      type="submit"
-      block
-      color="success"
-      size="large"
-      variant="elevated"
-      class="mt-4"
-      >Send Form</v-btn
-    >
-  </v-form>
-  <CategoryTable v-if="submitting.submitted" />
+  <v-locale-provider :rtl="false">
+    <v-form lazy-validation ref="catForm" @submit.prevent="submitForm()">
+      <CategoryMain />
+      <v-btn
+        :loading="submitting.loading"
+        type="submit"
+        block
+        color="success"
+        size="large"
+        variant="elevated"
+        class="mt-4"
+        >Send Form</v-btn
+      >
+    </v-form>
+    <CategoryTable v-if="submitting.submitted" />
+  </v-locale-provider>
 </template>
 
 <style scoped></style>
